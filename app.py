ee02682e1899f1147af36a3e986811622b8902f0
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-07-25 04:53:48 -0400

    used jsonify & fetch to create a getcube function sending python data to javascript for it to use on the frontend.

--- app.py
+++ app.py
@@ -1,44 +1,33 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
-@app.route("/")
+    # def rotate(movement):
 
-def index():
-    return render_template("index.html")
+    # 18 possible rotations: 
+    # Up: U, U', U^2
+    # Down: D, D', D^2
+    # Left: L, L', L^2+
+    # Right: R, R', R^2
+    # Front: F, F', F^2
+    # Back: B, B', B^2
 
-
-class RubiksCube:
-    def __init__(self):
-        # 0 for white, 1 for yellow, 2 for orange, 3 for green, 4 for red, 5 for blue 
-        # reference picture for each piece's index.
-        self.cube = [
-            0, 0, 0, 0, 0, 0, 0, 0, 0,  # White (top) face
-            1, 1, 1, 1, 1, 1, 1, 1, 1,  # Yellow (bottom) face
-            2, 2, 2, 2, 2, 2, 2, 2, 2,  # Orange (left) face
-            3, 3, 3, 3, 3, 3, 3, 3, 3,  # Green (front) face
-            4, 4, 4, 4, 4, 4, 4, 4, 4,  # Red (right) face
-            5, 5, 5, 5, 5, 5, 5, 5, 5   # Blue (back) face
-        ]
-
-    def rotate(movement):
-        # 18 possible rotations: 
-            '''
-            Up: U, U', U^2
-            Down: D, D', D^2
-            Left: L, L', L^2
-            Right: R, R', R^2
-            Front: F, F', F^2
-            Back: B, B', B^2
-            '''
-
-    def reset():
-        cube = [
+cube = [ # 0 for white, 1 for yellow, 2 for orange, 3 for green, 4 for red, 5 for blue 
             0, 0, 0, 0, 0, 0, 0, 0, 0, 
             1, 1, 1, 1, 1, 1, 1, 1, 1, 
             2, 2, 2, 2, 2, 2, 2, 2, 2, 
             3, 3, 3, 3, 3, 3, 3, 3, 3, 
             4, 4, 4, 4, 4, 4, 4, 4, 4, 
             5, 5, 5, 5, 5, 5, 5, 5, 5
-         ]
-    
-        
+        ]
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(): 
+    return render_template("index.html")
+
+@app.route("/get_cube")
+def get_cube():
+    return jsonify(cube = cube)
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ static/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetchCube();
+});
+
+function fetchCube() {
+    console.log("Fetching cube data...");
+    fetch("/get_cube")
+        .then(response => response.json())
+        .then(data => {
+            renderCube(data.cube);
+            console.log("Fetched!")
+        })
+        .catch(error => console.error('Error fetching cube data:', error));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchCube();
+});
