37e9009b142dc45052443a6fc477ea985d6a1fb7
Ahmed Alamin <ahmedalamin957@gmail.com>
2024-07-25 21:08:42 -0400

    created displaycube function in js & tweaked html/css files to display rubix cube

--- static/script.js
+++ static/script.js
@@ -7,7 +7,26 @@ function fetchCube() {
     fetch("/get_cube")
         .then(response => response.json())
         .then(data => {
-            console.log("Fetched!")
+            console.log("Fetched!");
+            displayCube(data.cube);
         })
         .catch(error => console.error('Error fetching cube data:', error));
+}
+
+
+function displayCube(cube){
+    const colors = ["white", "yellow", "orange", "green", "red", 'blue'];
+
+    for (let i = 0; i < colors.length; i++){
+        const faceGrid = document.getElementById(colors[i]);
+
+        for (let j = 0; j < 9; j++){
+            const cell = document.createElement('div');
+            const index = i * 9 + j;
+            cell.style.backgroundColor = colors[cube[index]];
+            cell.style.border = '1px solid black';
+            faceGrid.appendChild(cell);
+        }
+    }
+    console.log("Displayed!");
 }
